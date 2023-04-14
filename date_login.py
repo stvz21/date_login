f41e5b20b87516740d7459b181fafd8435c6c598
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-04-14 16:59:04 -0400

    Update date_login.py

--- date_login.py
+++ date_login.py
@@ -1,1 +1,1 @@
-{"id": 29412, "user": "stvz02", "pasw": "stvz02", "down": "https://apye.esceg.cu/index.php/apye", "host": "https://apye.esceg.cu/index.php/apye/login/signIn"}
+{"id": 124, "user": "stvz02", "pasw": "stvz02", "down": "https://ediciones.uo.edu.cu/index.php/e1", "host": "https://ediciones.uo.edu.cu/index.php/e1/login/signIn"}
